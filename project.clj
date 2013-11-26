488910599181628cde951434a617c35b60c24e81
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-11-26 03:08:21 +0200

    Fixed comment url and pulling

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reddit.clj "0.4.1"
+(defproject reddit.clj "0.4.1-fakedrake"
   :description "A reddit API wrapper for clojure"
   :author "Sun Ning <classicning@gmail.com>"
   :dependencies [[org.clojure/clojure "1.4.0"]
